8e55b6b5d3532ec3f009d87e338794560a4bbe77
Jorge Avelar <jorgecavelar@gmail.com>
2014-10-15 00:51:00 -0300

    change emacs theme and bash_prompt color

--- .emacs.d/init/init-themes.el
+++ .emacs.d/init/init-themes.el
@@ -1,13 +1,10 @@
 ;;; init-themes.el
 
-(require-package 'solarized-theme)
-(use-package solarized-theme
+(require-package 'soft-morning-theme)
+(use-package soft-morning-theme
   :config
   (progn
-    (setq solarized-high-contrast-mode-line t)
-    (setq solarized-distinct-fringe-background t)
-    (load-theme 'solarized-dark t)
-    (set-face-attribute 'helm-selection nil :underline nil)))
+    (load-theme 'soft-morning t)))
 
 (provide 'init-themes)
 
